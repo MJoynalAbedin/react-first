617ae9f30f18cfb4481f7f59d5bc27854a1204b5
MJoynalAbedin <mdakibctg140@gmail.com>
2021-07-04 14:21:31 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Button, Navbar, Form, InputGroup, FormControl} from 'react-bootstrap';
+import Chart from './components/Chart.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>My React Works</p>
-      </header>
+
+    return (
+    <div>
+        {/* <h1 className="text-center">Hi I am Akib</h1>
+        <Button variant='primary'>Primary</Button>
+        <Navbar className="bg-light justify-content-between">
+          <Form inline>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Username"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Form>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Navbar> */}
+
+        <Chart />
     </div>
   );
 }
